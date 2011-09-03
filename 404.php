f84cf688b6b4f70466f9c68b8349e5e47e613c36
Steve King <steve.king5891@gmail.com>
2011-09-03 11:57:25 +0100

    Added 404.php

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+
+<div class="main-col">
+		
+		<article class="post">
+			<header>
+				<h2>404 - Not found</h2>
+	    </header>
+	    <p>Sorry, something went wrong. We couldn't find the page you were looking for.</p>
+		</article>
+	
+</div><!-- END .main-col -->
+
+<?php get_sidebar();?>
+<?php get_footer();?>
