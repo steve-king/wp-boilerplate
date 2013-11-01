147a4eae5c9d975d5a84e38a7b7aec4e87490ae4
Steve King <steve.king@thisisdare.com>
2013-11-01 17:27:09 +0000

    updated to latest jquery, remove ruby config, changed scss folder structure

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
     <meta name="viewport" content="width=device-width">
 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
-
+    
+    <script src="<?php bloginfo('template_url')?>/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
